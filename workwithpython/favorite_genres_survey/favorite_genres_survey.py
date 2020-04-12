9b917e92ad160d0e6216f71a939e7676c9028b3b
morteza khoshsoroor <khoshsoroorm@gmail.com>
2020-04-12 18:51:30 +0430

    Finaly

--- workwithpython/favorite_genres_survey/favorite_genres_survey.py
+++ workwithpython/favorite_genres_survey/favorite_genres_survey.py
@@ -1,24 +1,28 @@
 import operator
 
+
 def fave(n):
     my = {}
     my_list = []
     for i in range(1, n + 1):
         m = input()
         m = m.lower().strip().split()
-        for i in range(1,len(m)):
-
+        for i in range(1, len(m)):
             my_list.append(m[i])
     for item in my_list:
         if item != my:
             my[item] = my_list.count(item)
-    my_value = my.values()
-    my_keys = my.keys()
-    final_dict = {}
-# ta inja faqat monde sort kardan dictionary
+    output_dict = {}
+    for key in sorted(my.keys()):
+        output_dict[key] = my[key]
+
+    sorted_d = dict(sorted(output_dict.items(), key=operator.itemgetter(1), reverse=True))
+
+    for item in sorted_d:
+        print(item.title(), ':', sorted_d[item])
 
 
-    return my
+n = int(input())
 
 
-print(fave(4))
+fave(n)
